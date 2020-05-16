cc0bbe0c151558a4f1831dfb820193991149b058
LimHanGyeol <dlagksruf19@naver.com>
2020-05-16 17:55:33 +0900

    EatGo-Restaurant-api : Store Detail

---
+++ eatgo-api/src/main/java/me/hangyeol/eatgo/domain/RestaurantRepository.java
@@ -0,0 +1,25 @@
+package me.hangyeol.eatgo.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantRepository {
+
+    private List<Restaurant> restaurantList = new ArrayList<>();
+
+    public RestaurantRepository() {
+        restaurantList.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        restaurantList.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+    }
+
+    public List<Restaurant> findAll() {
+        return restaurantList;
+    }
+
+    public Restaurant findById(Long id) {
+        return restaurantList.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
--- eatgo-api/src/main/java/me/hangyeol/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/me/hangyeol/eatgo/interfaces/RestaurantController.java
@@ -1,7 +1,10 @@
 package me.hangyeol.eatgo.interfaces;
 
 import me.hangyeol.eatgo.domain.Restaurant;
+import me.hangyeol.eatgo.domain.RestaurantRepository;
+import org.apache.catalina.util.ErrorPageSupport;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,15 +13,21 @@ import java.util.List;
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository restaurantRepository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurantList = new ArrayList<>();
+        List<Restaurant> restaurantList = restaurantRepository.findAll();
 
-        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
-        restaurantList.add(restaurant);
         return restaurantList;
     }
 
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        Restaurant restaurant = restaurantRepository.findById(id);
+
+        return restaurant;
+    }
 
 
 }
