6c0ef30bc6b94192177c369411aff76a74e2231e
LimHanGyeol <dlagksruf19@naver.com>
2020-06-04 22:33:23 +0900

    EatGo-Restaurant-api : Menu Management

---
+++ eatgo-api/src/main/java/me/hangyeol/eatgo/application/MenuItemService.java
@@ -0,0 +1,15 @@
+package me.hangyeol.eatgo.application;
+
+import me.hangyeol.eatgo.domain.MenuItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
+        // TODO: bulk update
+    }
+}
---
+++ eatgo-api/src/main/java/me/hangyeol/eatgo/interfaces/MenuItemController.java
@@ -0,0 +1,29 @@
+package me.hangyeol.eatgo.interfaces;
+
+import me.hangyeol.eatgo.application.MenuItemService;
+import me.hangyeol.eatgo.domain.MenuItem;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MenuItemController {
+
+    private MenuItemService menuItemService;
+
+    public MenuItemController(MenuItemService menuItemService) {
+        this.menuItemService = menuItemService;
+    }
+
+    @PatchMapping("/restaurants/{restaurantId}/menuitems")
+    public String bulkUpdate(@PathVariable("restaurantId") Long restaurantId,
+                             @RequestBody List<MenuItem> menuItems) {
+        menuItemService.bulkUpdate(restaurantId, menuItems);
+        return "";
+    }
+
+}
