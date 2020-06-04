ce693e932d74c673f9a3e09f1064c42c4754ca6b
LimHanGyeol <dlagksruf19@naver.com>
2020-06-04 23:05:01 +0900

    EatGo-Restaurant-api : Menu Management2

--- eatgo-api/src/main/java/me/hangyeol/eatgo/application/MenuItemService.java
+++ eatgo-api/src/main/java/me/hangyeol/eatgo/application/MenuItemService.java
@@ -1,6 +1,7 @@
 package me.hangyeol.eatgo.application;
 
 import me.hangyeol.eatgo.domain.MenuItem;
+import me.hangyeol.eatgo.domain.MenuItemRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +9,16 @@ import java.util.List;
 @Service
 public class MenuItemService {
 
+    private MenuItemRepository menuItemRepository;
+
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
 
     public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
-        // TODO: bulk update
+        for (MenuItem menuItem : menuItems) {
+            menuItem.setRestaurantId(restaurantId);
+            menuItemRepository.save(menuItem);
+        }
     }
 }
