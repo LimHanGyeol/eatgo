43e6f33305ccada94e2152d2e99b73e563dc98b0
LimHanGyeol <dlagksruf19@naver.com>
2020-06-16 23:54:42 +0900

    EatGo-Restaurant-api : Filtering Shop List

---
+++ eatgo-admin-api/src/test/java/me/hangyeol/eatgo/interfaces/RegionControllerTests.java
@@ -0,0 +1,33 @@
+package me.hangyeol.eatgo.interfaces;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(RegionController.class)
+class RegionControllerTests {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/regions"))
+                .andExpect(status().isOk());
+    }
+
+}
--- eatgo-common/src/main/java/me/hangyeol/eatgo/domain/Restaurant.java
+++ eatgo-common/src/main/java/me/hangyeol/eatgo/domain/Restaurant.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Restaurant {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     @Setter
     private Long id;
 
